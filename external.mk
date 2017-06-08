9ffdf0121d7ec31362b8bbdf3fe3162ce3ce2955
Bert Outtier <bouttier@nalys-group.com>
2017-06-08 08:28:53 +0200

    Initial commit

---
+++ external.mk
@@ -0,0 +1,22 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_PIPRINT_BUILDROOT_BUILDROOT_PATH)/package/*/*.mk))
+
+flash-rpi:
+	@if ! test $(DRIVE); then \
+		echo "No drive specified. Please specify you SD card using DRIVE=<your sd card device>"; \
+		exit 1; \
+	fi
+
+	@for PARTITION in 1 2; do \
+		if [ -b $(DRIVE)$${PARTITION} ]; then \
+			echo unmounting $(DRIVE)$${PARTITION}; \
+			umount $(DRIVE)$${PARTITION}; \
+		fi; \
+	done
+
+	@if [ -b $(DRIVE) ]; then \
+		echo Writing $(img) to sd card $(DRIVE); \
+		sudo dd if=$(img) of=$(DRIVE); \
+	else \
+		echo "$(DRIVE) is not a block device"; \
+		exit 1; \
+	fi
